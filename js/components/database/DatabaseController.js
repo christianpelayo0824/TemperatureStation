7ca7b9de6c5d2ba9ab6125c539fb77b012c1480b
Your Name <you@example.com>
2018-10-18 20:42:23 +0800

    First init of version 1

--- js/components/database/DatabaseController.js
+++ js/components/database/DatabaseController.js
@@ -1,3 +1,16 @@
-mainApp.controller('DatabaseController', function($scope){
-    
+mainApp.controller('DatabaseController', function ($scope, TemperatureService, $interval) {
+
+    getTemperature = function () {
+        $interval(function () {
+            TemperatureService.getAllTemperature()
+                .then(function (response) {
+                    $scope.temperature = [];
+                    $scope.temperature = response.data.data;
+                });
+        }, 2000)
+
+    }
+
+    getTemperature();
+
 })
